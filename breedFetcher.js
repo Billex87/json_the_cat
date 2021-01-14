00ded15fe82d4a3e642a197304f3252fe12ef3e1
Billy <billy_mckinnon@hotmail.com>
2021-01-14 18:32:30 +0000

    Refactored

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,16 +1,26 @@
 const request = require('request');
-let breed = process.argv.slice(2);
-request(`https://api.thecatapi.com/v1/breeds/search?q=${breed}`, function(error, response, body) {
-  if (body === '[]') {
-    console.log("The breed is not found.");
-  } if (error !== null) {
-    console.log("Wrong URL ya bum");
-  }
-  // console.error('error:', error); // Print the error if one occurred
-  // console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-  // console.log('body:', typeof body); // Print the HTML for the Google homepage.
-  // const data = JSON.parse(body);
-  // console.log(data);
-  // console.log(typeof data);
-  // console.log(body);
-});
+
+const fetchBreedDescription = function(breed, callback) {
+  request('https://api.thecatapi.com/v1/breeds/search?q=' + breed, (error, response, body) => {
+    //if something is wrong in general
+    if (error !== null) {
+      callback(error, null);
+    } else {
+      const data = JSON.parse(body);
+      if (data[0] === undefined || data[0].length === 0) {
+        callback(error, null);
+      } else {
+        callback(null, data[0]['description']);
+      }
+    }
+  });
+};
+
+module.exports = { fetchBreedDescription };
+// console.error('error:', error); // Print the error if one occurred
+// console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+// console.log('body:', typeof body); // Print the HTML for the Google homepage.
+// const data = JSON.parse(body);
+// console.log(data);
+// console.log(typeof data);
+// console.log(body);
